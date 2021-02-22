cec3c0ddb454929dcccaa2c922356caccad382e4
sabina <sabieena>
2021-02-22 15:30:53 +0545

    new sails project

---
+++ api/controllers/UserController.js
@@ -0,0 +1,39 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+  /**
+   * `UserController.login()`
+   */
+  login: async function (req, res) {
+    return res.json({
+      todo: 'login() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `UserController.logout()`
+   */
+  logout: async function (req, res) {
+    return res.json({
+      todo: 'logout() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `UserController.signup()`
+   */
+  signup: async function (req, res) {
+    return res.json({
+      todo: 'signup() is not implemented yet!'
+    });
+  }
+
+};
+
